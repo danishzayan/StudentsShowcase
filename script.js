7b57ea8a88dc8daec8422f65be20238eaf7f36a0
Sree Vyshnavi <sreevysh2002@gmail.com>
2023-10-02 12:04:56 +0530

    display msg button only if a mail is given

--- script.js
+++ script.js
@@ -9,36 +9,63 @@ function displayUserContributions() {
     card.classList.add("card-box");
     // console.log("user data is => ", userProfiles);
     // Create the profile card content
-    card.innerHTML = `
-            <span class="pro">${profile.course}</span>
-            <img class="round" src="${profile.imageUrl}" alt="user" />
-            <h3>${profile.name}</h3>
-            <h6>${profile.city}</h6>
-            <p>${profile.work}</p>
-            <div class="buttons">
-                <a href="${
-                  profile.msgURL
-                }" target="_blank" rel="noopener noreferrer">
-                  <button class="primary">
-                    Message
-                   </button>
-                </a>
-                <a href="${
-                  profile.linkedinURL
-                }" target="_blank" rel="noopener noreferrer">
-                   <button class="primary ghost">
-                    Hire Me
-                   </button>
-                </a>
-            </div>
-            <div class="skills">
-                <h6 class="skill-heading">Skills</h6>
-                <ul>
-                    ${profile.skills.map((skill) => `<li>${skill}</li>`)}
-                </ul>
-            </div>
-        `;
 
+    //if no valid url is entered for msg
+    if (profile.msgURL) {
+      card.innerHTML = `
+      <span class="pro">${profile.course}</span>
+      <img class="round" src="${profile.imageUrl}" alt="user" />
+      <h3>${profile.name}</h3>
+      <h6>${profile.city}</h6>
+      <p>${profile.work}</p>
+      <div class="buttons">
+      <a href="mailto:${
+        profile.msgURL
+      }" target="_blank" rel="noopener noreferrer">
+      <button class="primary">
+      Message
+      </button>
+      </a>
+      <a href="${
+        profile.linkedinURL
+      }" target="_blank" rel="noopener noreferrer">
+                   <button class="primary ghost">
+                   Hire Me
+                   </button>
+                   </a>
+                   </div>
+                   <div class="skills">
+                   <h6 class="skill-heading">Skills</h6>
+                   <ul>
+                   ${profile.skills.map((skill) => `<li>${skill}</li>`)}
+                   </ul>
+                   </div>
+                   `;
+    } else {
+      card.innerHTML = `
+      <span class="pro">${profile.course}</span>
+      <img class="round" src="${profile.imageUrl}" alt="user" />
+      <h3>${profile.name}</h3>
+      <h6>${profile.city}</h6>
+      <p>${profile.work}</p>
+      <div class="buttons">
+      
+      <a href="${
+        profile.linkedinURL
+      }" target="_blank" rel="noopener noreferrer">
+                   <button class="primary ghost">
+                   Hire Me
+                   </button>
+                   </a>
+                   </div>
+                   <div class="skills">
+                   <h6 class="skill-heading">Skills</h6>
+                   <ul>
+                   ${profile.skills.map((skill) => `<li>${skill}</li>`)}
+                   </ul>
+                   </div>
+                   `;
+    }
     // Append the profile card to the container
     cardContainer.appendChild(card);
   });
